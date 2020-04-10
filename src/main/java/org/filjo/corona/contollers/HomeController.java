19dbd0e72623a1070963cdcb02039fdae3b84536
Philipp <55308835+Filjo0@users.noreply.github.com>
2020-04-10 18:19:21 +0800

    Initial commit

---
+++ src/main/java/org/filjo/corona/contollers/HomeController.java
@@ -0,0 +1,28 @@
+package org.filjo.corona.contollers;
+
+import org.filjo.corona.models.LocationStats;
+import org.filjo.corona.services.CoronaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaService coronaService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
