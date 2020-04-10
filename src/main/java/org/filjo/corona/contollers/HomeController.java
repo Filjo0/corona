a23c21d4f6a0d5fe36c0dd782e37f40c299de48f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-10 16:45:57 +0000

    Deploy Filjo0/corona to github.com/Filjo0/corona.git:gh-pages

--- src/main/java/org/filjo/corona/contollers/HomeController.java
+++ src/main/java/org/filjo/corona/contollers/HomeController.java
@@ -1,6 +1,7 @@
 package org.filjo.corona.contollers;
 
 import org.filjo.corona.models.LocationStats;
+import org.filjo.corona.services.CoronaDeathService;
 import org.filjo.corona.services.CoronaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,13 +17,32 @@ public class HomeController {
     CoronaService coronaService;
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String greeting() {
+        return "index";
+    }
+
+    @GetMapping("/cases")
+    public String cases(Model model) {
         List<LocationStats> allStats = coronaService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
         int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
-        return "home";
+        return "cases";
+    }
+
+    @Autowired
+    CoronaDeathService coronaDeathService;
+
+    @GetMapping("/death")
+    public String death(Model model) {
+        List<LocationStats> allStats = coronaDeathService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "death";
     }
 }
